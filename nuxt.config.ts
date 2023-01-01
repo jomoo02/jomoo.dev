fdabe45ab190c33d21f6ab594cd7e264ecabd69f
jomoo02 <jomootgrn88@naver.com>
2023-01-01 23:52:29 +0900

    post: new post 2023/01/01

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
         head:{
             title:'jomoo.dev',
             meta:[
-                {name: 'description', content: 'Nuxt3로 만들어진 블로그, 프로그래머스, 알고리즘 등을 다룹니다'}
+                {name: 'description', content: 'Nuxt3로 만들어진 블로그, 프로그래머스, 알고리즘 등을 다룹니다'},
+                {name: "google-site-verification", content: "pUBmMyGew19WMcC1R--OrdY4dh0W7GQapORYYaxecWY"}
             ]
         }
     },
