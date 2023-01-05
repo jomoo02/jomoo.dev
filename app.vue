34ce105f33c1999a585a61c6f32130ddda63862c
jomoo02 <jomootgrn88@naver.com>
2023-01-06 00:24:37 +0900

    REFACTOR: Store.postData

--- app.vue
+++ app.vue
@@ -6,3 +6,10 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import { usePostDataStore } from '~~/store/postData';
+const postStore = usePostDataStore();
+const posts =  await queryContent('note/programmers').only(['title','_path','description','date']).find();
+postStore.programmersPostUpdate(posts);
+</script>
--- pages/note/programmers/index.vue
+++ pages/note/programmers/index.vue
@@ -18,8 +18,7 @@
 <script setup>
 import { usePostDataStore } from '~~/store/postData';
 const postStore = usePostDataStore();
-const posts =  await queryContent('note/programmers').only(['title','_path','description','date']).find();
-postStore.programmersPostUpdate(posts);
+
 
 useHead({
     title: 'jomoo.dev 프로그래머스',
