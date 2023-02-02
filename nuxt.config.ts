50ea6daff9d9572726c0acd4c1460311e2ef74f0
jomoo02 <jomootgrn88@naver.com>
2023-02-03 00:57:23 +0900

    POST: projects vocabulary post 3. 단어 삭제 글 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,18 +13,12 @@ export default defineNuxtConfig({
             ]
         }
     },
-    // extends:['nuxt-seo-kit'],
-    // runtimeConfig: {
-    //     public: {
-    //         siteUrl: 'https://jomoo-dev.vercel.app/',
-    //         siteName: 'Jomoo Dev',
-    //         siteDescription: 'Nuxt3로 만들어진 블로그, 프로그래머스, 알고리즘 등을 다룹니다',
-    //     }
-    // },
+
     modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@pinia/nuxt','nuxt-icon'],
     content:{
         highlight:{
-            preload:['python','py']
+            preload:['python','py','vue'],
+            theme: 'dracula',
         }
     },
     plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
