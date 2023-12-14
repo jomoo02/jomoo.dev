5ec3859ede95ff0efbff6b08f07b7c5b95adfb36
jomoo02 <jomootgrn88@naver.com>
2023-12-15 01:49:23 +0900

    post: projects/vocabularynote/refactor-3 초안 작성

--- pages/projects/vocabularynote/index.vue
+++ pages/projects/vocabularynote/index.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
         <div class="relative grid grid-cols-10 gap-6 md:gap-8">
-            <div class="col-span-10 md:col-span-2 flex md:sticky top-[138px] overflow-y-auto max-h-[calc(100vh-220px)] ">
-                <ProjectsSideBar></ProjectsSideBar>
+            <div class="col-span-10 md:col-span-2 flex md:sticky top-[138px] overflow-y-auto max-h-[calc(100vh-220px)]">
+                <ProjectsSideBar />
             </div>
             <div class="col-span-10 md:col-span-8">
+                <div class="flex justify-end">
+                    <NuxtLink to="https://github.com/jomoo02/VocabularyNote" target="_blank">
+                        <span class="bg-slate-300 py-1 xs:px-2 xs:text-sm text-base px-4 rounded-lg font-semibold text-zinc-700 opacity-80">프로젝트 GitHub 링크</span>
+                    </NuxtLink>
+                </div>
                 <div v-for="voca in postStore.vocaPosts" :key="voca">
                     <div>
-                        <ContentCard :page-link="voca._path" :description="voca.description" :title="voca.title" :date="voca.date" ></ContentCard>
+                        <ContentCard :page-link="voca._path" :description="voca.description" :title="voca.title" :date="voca.date" />
                     </div>
                 </div>
             </div>
