4b65c96be7a64e525ee5ceefb410fef5942cbce4
jomoo02 <jomootgrn88@naver.com>
2024-01-19 22:13:14 +0900

    feat: creationDate and category, detail in head
    creationDate 컴포넌트로 날짜 형식 변환 후 렌더링
    content head에 category, detail 추가

---
+++ components/content/CreationDate.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  date: {
+    type: String,
+    required: true,
+  },
+});
+
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+const formatDate = computed(() => {
+  const date = new Date(props.date);
+  return date.toLocaleDateString('ko-KR', options);
+});
+</script>
+
+<template>
+  <div class="flex justify-end text-sm px-2">{{ formatDate }}</div>
+</template>
